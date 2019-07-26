13292b83e64f3264f6fff28f713cef49f9b60b09
mcguia <amcg10@gmail.com>
2019-07-26 17:08:26 -0400

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,21 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const dotenv = require("dotenv");
+
+if (process.env.ENVIRONMENT !== "production") {
+    dotenv.config();
+}
+
+const { spaceId, accessToken } = process.env;
+
 
 module.exports = {
-  /* Your site config here */
-}
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+        resolve: "gatsby-source-contentful",
+        options: {
+            spaceId,
+            accessToken
+        }
+    }
+  ]
+};
