e91dce07f3a738e877eec5a54f44f58432cce5da
Austin McGuire <amcg10@gmail.com>
2020-01-30 13:51:27 -0500

    add headers, manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,12 +31,55 @@ module.exports = {
       },
     },
     "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Austin McGuire",
+        short_name: "AustinMcGuire",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#b3b9c4",
+        display: "minimal-ui",
+        icon: "src/images/logo.png",
+      },
+    },
     "gatsby-plugin-offline",
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-layout",
       options: {
         component: require.resolve(`./src/components/Layout.js`),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/public/**/*.html": [
+            "cache-control: public, max-age=0, must-revalidate",
+          ],
+          "/*.css": [
+            "cache-control: public, max-age=31536000, immutable",
+          ],
+          "/*.js": [
+            "cache-control: public, max-age=31536000, immutable",
+          ],
+          "/sw.js": [
+            "cache-control: public, max-age=0, must-revalidate",
+          ],
+          "/public/page-data/*": [
+            "cache-control: public, max-age=0, must-revalidate",
+          ],
+          "/static/*": [
+            "cache-control: public, max-age=31536000, immutable",
+          ],
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
       },
     },
     "gatsby-transformer-remark",
